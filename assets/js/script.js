11429ca37ce623b15a276c44bb70dee6c8e91267
rodrigosebastian-ca <rodrigosebastian.ca@gmail.com>
2026-03-27 03:17:04 -0300

    Se agregaron nueva cards pero faltan detalles de información, imagenes y códigos. Se ordeno código y se arrelgaron fallas de tipeo y cierres de elementos

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+
+
+
+let contador = 0;
+    const agregarProducto = () => {
+          contador++;
+    const visorContador = document.getElementById("suma-del-carrito");
+          visorContador.innerText = contador;
+          console.log("Productos en carrito:", contador);
+                                  };
+
