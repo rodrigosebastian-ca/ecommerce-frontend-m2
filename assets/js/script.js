5fc240988f3abc6270f5eb2ac424c1431a23eb35
rodrigosebastian-ca <rodrigosebastian.ca@gmail.com>
2026-03-28 02:42:33 -0300

    Finalización de portafolio módulo 2 e-commerce, se terminaron últimos arreglos semánticos HTML5 obtetiendo mejor estructura, se validó el archivo HTML por validator.w3.org y se arreglaron peligros variados de cierre de elementos y de faltas de títulos por lo que se agregaron clases de titulos visually-hidden para usuarios no videntes y mantener la jerarquía y estructura correspondiente

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 
-
-
+// Contador de carrito //
 
 let contador = 0;
-    const agregarProducto = () => {
-          contador++;
-    const visorContador = document.getElementById("suma-del-carrito");
-          visorContador.innerText = contador;
-          console.log("Productos en carrito:", contador);
-                                  };
+  const agregarProducto = () => {
+      contador++;
+  const visorContador = document.getElementById("suma-del-carrito");
+  visorContador.innerText = contador;
+  console.log("Productos dentro del carrito:", contador);
+};
+
 
